fa71d3b8782ddd9a9939e678025ca4f95528eceb
JuanCarlos24691 <juancarlosospinovizcaino@hotmail.com>
2026-04-27 14:17:50 -0500

    update

--- TheEconomy.Server/Program.cs
+++ TheEconomy.Server/Program.cs
@@ -1,16 +1,19 @@
-﻿using System.IO;
-using SampSharp.Core;
+﻿using SampSharp.Core;
 using SampSharp.Entities;
+using System.Text;
 
-namespace TheEconomy.Server
+namespace TheEconomy.Server;
+
+public class Program
 {
-    public class Program
+    public static void Main()
     {
-        public static void Main() =>
-           new GameModeBuilder()
-           .UseEcs<Startup>()
-           .UseEncoding(Path.Combine($"{Directory.GetCurrentDirectory()}/../Encoding/", "8859-1.txt"))
-           .RedirectConsoleOutput()
-           .Run();
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        new GameModeBuilder()
+            .UseEcs<Startup>()
+            .UseEncoding(Encoding.GetEncoding("iso-8859-1"))
+            .RedirectConsoleOutput()
+            .Run();
     }
 }
